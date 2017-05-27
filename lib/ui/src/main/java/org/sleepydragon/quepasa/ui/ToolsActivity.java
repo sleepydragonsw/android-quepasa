d68b09f4c6c080b471df3f5c4106595bf1358a5d
denver <denver@sleepydragon.org>
2017-05-27 09:53:29 -0400

    ui module added

---
+++ lib/ui/src/main/java/org/sleepydragon/quepasa/ui/ToolsActivity.java
@@ -0,0 +1,9 @@
+package org.sleepydragon.quepasa.ui;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * An Activity that is never used in code but is used by the layout editor for the theme.
+ */
+public final class ToolsActivity extends AppCompatActivity {
+}
