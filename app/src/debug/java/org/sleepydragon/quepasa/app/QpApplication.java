a6a87518d87b3a8ab6f61929eb1f4f2948af7775
denver <denver@sleepydragon.org>
2017-05-27 10:20:21 -0400

    MultiDex enabled on debug builds

---
+++ app/src/debug/java/org/sleepydragon/quepasa/app/QpApplication.java
@@ -0,0 +1,6 @@
+package org.sleepydragon.quepasa.app;
+
+import android.support.multidex.MultiDexApplication;
+
+public final class QpApplication extends MultiDexApplication {
+}
---
+++ app/src/release/java/org/sleepydragon/quepasa/app/QpApplication.java
@@ -0,0 +1,6 @@
+package org.sleepydragon.quepasa.app;
+
+import android.app.Application;
+
+public final class QpApplication extends Application {
+}
